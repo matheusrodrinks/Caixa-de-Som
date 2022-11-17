8964461692d55c74834cecefa0c268c90802e79e
MATHEUS RODRIGUES MENDONÇA <82005382+matheusrodrinks@users.noreply.github.com>
2022-11-17 18:41:58 -0400

    atualização

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
 }
 
-const listaDeTeclas = [document.querySelectorAll('.tecla')]
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+while (contador < listaDeTeclas.lenght) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    
+    contador= contador + 1
+
+    console.log(contador);
+}
+
+
 
 
 
